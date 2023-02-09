404969adb6b2483b696ff7fa9d6f65e7ce98cdba
thedanisaur <definisdan@yahoo.com>
2023-02-09 18:47:02 -0500

    Change default passwords

--- data_load/insert_people.sql
+++ data_load/insert_people.sql
@@ -1,3 +1,2 @@
-INSERT INTO people (person_id, person_username, person_password, person_email, person_created_on) VALUES (UUID_TO_BIN(UUID()), 'nick', '$2a$12$liSJKHfWvdTxrkT0lgT7MubIJ3h/WHQVjeT4xydzgVa8CO/GVlKZW', 'turtleishnick@yahoo.com', '2015-07-01');
-INSERT INTO people (person_id, person_username, person_password, person_email, person_created_on) VALUES (UUID_TO_BIN(UUID()), 'dan', '$2a$12$7hIZiaz5qCPqu3SwftP4w.OGn3Pp2Kl1UEhuOTFIRQxWbwB0kHg4S', 'definisdan@yahoo.com', '2015-07-01');
-
+INSERT INTO people (person_id, person_username, person_password, person_email, person_created_on) VALUES (UUID_TO_BIN(UUID()), 'nick', '$2a$12$S9rJoqk/ylp9nVfdLPJvmue2zJhBFtz/EEhpTcFmnMtJtetproJQu', 'turtleishnick@yahoo.com', '2015-07-01');
+INSERT INTO people (person_id, person_username, person_password, person_email, person_created_on) VALUES (UUID_TO_BIN(UUID()), 'dan', '$2a$12$cxFQ9duA8WF68ax9jxJYjua8C81MfArGPTm1j4xY0x/VvJsA8wF2C', 'definisdan@yahoo.com', '2015-07-01');
