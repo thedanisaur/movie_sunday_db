d570c48d8cd7e10de6bdab03dd6d181c029f71aa
Daniel DeFinis <definisdan@yahoo.com>
2025-06-13 22:54:01 -0500

    Fix auto increment on series table

--- tables/create_series.sql
+++ tables/create_series.sql
@@ -1,6 +1,5 @@
 CREATE TABLE series (
-    -- TODO This auto increments even when failing to insert a row...ordering can't have gaps
-     series_order int NOT NULL AUTO_INCREMENT PRIMARY KEY
+     series_order INT NOT NULL PRIMARY KEY
     , series_name VARCHAR(255) NOT NULL
     , series_title TEXT NOT NULL
     , series_created_on DATE NOT NULL
@@ -11,3 +10,14 @@ CREATE TABLE series (
         ON UPDATE NO ACTION ON DELETE NO ACTION
     , UNIQUE (series_name)
 );
+
+DROP TRIGGER IF EXISTS bi_series;
+CREATE TRIGGER bi_series BEFORE INSERT ON series FOR EACH ROW
+BEGIN
+    DECLARE next_order INT;
+    -- TODO this is not thread safe
+    -- Get the next series_order value (max + 1, or 1 if table is empty)
+    SELECT COALESCE(MAX(series_order), 0) + 1 INTO next_order FROM series;
+    -- Always update the order to be our value
+    SET NEW.series_order = next_order;
+END;
